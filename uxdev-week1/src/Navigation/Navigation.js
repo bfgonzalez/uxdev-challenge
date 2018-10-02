87f2bc8623f6f6d54d53dc3772091c912753e3e3
bfgonzalez <bfgonzalez142@gmail.com>
2018-10-02 11:50:40 +0800

    added initial files for react app

---
+++ uxdev-week1/src/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Navigation.css';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div className="Navigation">
+      </div>
+    )
+  }
+}
+
+export default Navigation;
