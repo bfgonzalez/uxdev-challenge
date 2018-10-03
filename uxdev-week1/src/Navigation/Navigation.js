bcaf53249ebd05d3649a7abb4806e128bd496fe9
bfgonzalez <bfgonzalez142@gmail.com>
2018-10-03 14:37:57 +0800

    chore: turned nav items into individual components

--- uxdev-week1/src/Navigation/Navigation.js
+++ uxdev-week1/src/Navigation/Navigation.js
@@ -5,7 +5,14 @@ class Navigation extends Component {
   render() {
     return (
       <div className="Navigation">
-        <NavItem />
+        <NavItem
+          dashboard="Dashboard"
+          accounts="My Accounts"
+          partners="Sub Partners"
+          withdraw="Withdraw"
+          marketing="Marketing Tools"
+          reports="Reports"
+        />
       </div>
     )
   }
@@ -15,8 +22,12 @@ class NavItem extends Component {
   render() {
     return (
       <div>
-        <div className="NavItem">Dashboard</div>
-        <div className="NavItem">My Accounts</div>
+        <div className="NavItem">{this.props.dashboard}</div>
+        <div className="NavItem">{this.props.accounts}</div>
+        <div className="NavItem">{this.props.partners}</div>
+        <div className="NavItem">{this.props.withdraw}</div>
+        <div className="NavItem">{this.props.marketing}</div>
+        <div className="NavItem">{this.props.reports}</div>
       </div>
     )
   }
