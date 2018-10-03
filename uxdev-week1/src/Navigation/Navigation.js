27c8dbf62c46f5cb7c453306a06afdcbf3edba1d
bfgonzalez <bfgonzalez142@gmail.com>
2018-10-03 18:13:05 +0800

    made nav item components more reusable

--- uxdev-week1/src/Navigation/Navigation.js
+++ uxdev-week1/src/Navigation/Navigation.js
@@ -5,14 +5,12 @@ class Navigation extends Component {
   render() {
     return (
       <div className="Navigation">
-        <NavItem
-          dashboard="Dashboard"
-          accounts="My Accounts"
-          partners="Sub Partners"
-          withdraw="Withdraw"
-          marketing="Marketing Tools"
-          reports="Reports"
-        />
+        <NavItem label="Dashboard" />
+        <NavItem label="My Accounts" />
+        <NavItem label="Sub Partners" />
+        <NavItem label="Withdraw" />
+        <NavItem label="Marketing Tools" />
+        <NavItem label="Reports" />
       </div>
     )
   }
@@ -21,14 +19,7 @@ class Navigation extends Component {
 class NavItem extends Component {
   render() {
     return (
-      <div>
-        <div className="NavItem">{this.props.dashboard}</div>
-        <div className="NavItem">{this.props.accounts}</div>
-        <div className="NavItem">{this.props.partners}</div>
-        <div className="NavItem">{this.props.withdraw}</div>
-        <div className="NavItem">{this.props.marketing}</div>
-        <div className="NavItem">{this.props.reports}</div>
-      </div>
+      <div className="NavItem">{this.props.label}</div>
     )
   }
 }
