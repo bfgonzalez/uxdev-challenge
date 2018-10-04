8a87e0346ebfa63880c897788cd05afe714201b9
bfgonzalez <bfgonzalez142@gmail.com>
2018-10-04 12:10:27 +0800

    updated icons and fixed alignment for navbar items

--- uxdev-week1/src/Navigation/Navigation.js
+++ uxdev-week1/src/Navigation/Navigation.js
@@ -4,14 +4,37 @@ import './Navigation.css';
 class Navigation extends Component {
   render() {
     return (
-      <div className="Navigation">
-        <NavItem label="Dashboard" />
-        <NavItem label="My Accounts" />
-        <NavItem label="Sub Partners" />
-        <NavItem label="Withdraw" />
-        <NavItem label="Marketing Tools" />
-        <NavItem label="Reports" />
-      </div>
+      <nav className="Navigation">
+        <div className="nav-section">
+          <i class="fas fa-chart-area"></i>
+          <NavItem label="Dashboard"/>
+        </div>
+
+        <div className="nav-section">
+          <i class="fas fa-user-circle"></i>
+          <NavItem label="My Accounts" />
+        </div>
+
+        <div className="nav-section">
+          <i class="fas fa-handshake"></i>
+          <NavItem label="Sub Partners" />
+        </div>
+
+        <div className="nav-section">
+          <i class="fas fa-credit-card"></i>
+          <NavItem label="Withdraw" />
+        </div>
+
+        <div className="nav-section">
+          <i class="fas fa-chart-line"></i>
+          <NavItem label="Marketing Tools" />
+        </div>
+
+        <div className="nav-section">
+          <i class="fas fa-chart-bar"></i>
+          <NavItem label="Reports" />
+        </div>
+      </nav>
     )
   }
 }
