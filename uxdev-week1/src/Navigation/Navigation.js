783cf0100cb0fbaad8b8a711524d97b826f2b5fb
bfgonzalez <bfgonzalez142@gmail.com>
2018-10-06 21:34:08 +0800

    added main logo and branding on lower right corner

--- uxdev-week1/src/Navigation/Navigation.js
+++ uxdev-week1/src/Navigation/Navigation.js
@@ -5,37 +5,39 @@ class Navigation extends Component {
   render() {
     return (
       <nav className="Navigation">
+      <h4>FXLINKED</h4>
         <div className="nav-section">
-          <span>
-            <i class="fas fa-chart-area"></i>
+          <span className="yellow-corner">
+            <i className="fas fa-chart-area"></i>
             <NavItem label="Dashboard"/>
           </span>
         </div>
 
         <div className="nav-section">
-          <i class="fas fa-user-circle"></i>
+          <i className="fas fa-user-circle"></i>
           <NavItem label="My Accounts" />
         </div>
 
         <div className="nav-section">
-          <i class="fas fa-handshake"></i>
+          <i className="fas fa-handshake"></i>
           <NavItem label="Sub Partners" />
         </div>
 
         <div className="nav-section">
-          <i class="fas fa-credit-card"></i>
+          <i className="fas fa-credit-card"></i>
           <NavItem label="Withdraw" />
         </div>
 
         <div className="nav-section">
-          <i class="fas fa-chart-line"></i>
+          <i className="fas fa-chart-line"></i>
           <NavItem label="Marketing Tools" />
         </div>
 
         <div className="nav-section">
-          <i class="fas fa-chart-bar"></i>
+          <i className="fas fa-chart-bar"></i>
           <NavItem label="Reports" />
         </div>
+        <p>Powered by <span className="brand">FXLinked</span></p>
       </nav>
     )
   }
